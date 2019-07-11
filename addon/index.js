832d2d85c126290e57b7bd02deb68aa9a9cd5f77
Scott Vitale <scott@spigotlabs.com>
2019-07-11 12:50:39 -0600

    Upgrade to Ember 3.10

--- addon/index.js
+++ addon/index.js
@@ -1,7 +1,26 @@
-import { COUNTRIES_LIST, COUNTRIES_WITHOUT_ZIP_LIST, COUNTRIES_WITH_STATES_LIST } from './utils/countries-lists';
-import { getCountry, isCountryWithState, isCountryWithoutZip } from './utils/countries-properties';
-import { US_STATES_LIST, US_MILITARY_STATES_LIST, US_REQUIRING_CUSTOM_DECLARATION_STATES_LIST, CA_STATES_LIST, STATES_BY_COUNTRIES } from './utils/states-lists';
-import { getStatesForCountry, isCustomsDeclarationRequiredInUS, countryContainsState, getState } from './utils/states-properties';
+import {
+  COUNTRIES_LIST,
+  COUNTRIES_WITHOUT_ZIP_LIST,
+  COUNTRIES_WITH_STATES_LIST
+} from './utils/countries-lists';
+import {
+  getCountry,
+  isCountryWithState,
+  isCountryWithoutZip
+} from './utils/countries-properties';
+import {
+  US_STATES_LIST,
+  US_MILITARY_STATES_LIST,
+  US_REQUIRING_CUSTOM_DECLARATION_STATES_LIST,
+  CA_STATES_LIST,
+  STATES_BY_COUNTRIES
+} from './utils/states-lists';
+import {
+  getStatesForCountry,
+  isCustomsDeclarationRequiredInUS,
+  countryContainsState,
+  getState
+} from './utils/states-properties';
 
 export {
   COUNTRIES_LIST,
